c9097e8c292c30f2655e74d79324e6f19764b005
Henry Beedham <45268597+henrybeedham@users.noreply.github.com>
2024-05-14 08:28:54 +0000

    suicide

--- Program.cs
+++ Program.cs
@@ -7,4 +7,26 @@ class Program
         // Here is your empty program!
         Console.WriteLine("Hello world!");
     }
+
+    static void Feed(ref string state, ref int size) {
+        size++;
+        Console.WriteLine("Fish fed");
+        
+    }
 }
+
+// SUB feed(BYREF state, BYREF size);
+//    size  size + 1;
+//    OUTPUT "Fish fed";
+//    IF size = 5 THEN;
+//       state  "FISH";
+//    ENDIF;
+// ENDSUB;
+ 
+// thisFishState  "Fish";
+// thisFishSize  1;
+// OUTPUT thisFishState, " is of size ", thisFishSize;
+// WHILE thisFishState <> "FISH";
+//    feed(thisFishState, thisFishSize);
+// ENDWHILE;
+// OUTPUT "It is now a big ", thisFishState;
