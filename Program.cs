e07a771c658a3dab779d81bff027c8b2c984508a
Henry Beedham <45268597+henrybeedham@users.noreply.github.com>
2024-05-17 08:40:10 +0000

    🤷‍♂️

--- Program.cs
+++ Program.cs
@@ -4,16 +4,32 @@ class Program
 {
     static void Main()
     {
-        // Here is your empty program!
-        Console.WriteLine("Hello world!");
+        string thisFishState = "Fish";
+        int thisFishSize =1;
+        Console.WriteLine($"{thisFishState} is of size {thisFishSize}");
+        while (thisFishState != "FISH")
+        {
+            Feed(ref thisFishState, ref thisFishSize);
+        }
+        Console.WriteLine($"It is now a big {thisFishState}");
     }
 
     static void Feed(ref string state, ref int size) {
         size++;
         Console.WriteLine("Fish fed");
-        
+        if (size == 5) {
+            state = "FISH";
+        }
     }
 }
+
+// thisFishState  "Fish";
+// thisFishSize  1;
+// OUTPUT thisFishState, " is of size ", thisFishSize;
+// WHILE thisFishState <> "FISH";
+//    feed(thisFishState, thisFishSize);
+// ENDWHILE;
+// OUTPUT "It is now a big ", thisFishState;
 
 // SUB feed(BYREF state, BYREF size);
 //    size  size + 1;
@@ -23,10 +39,3 @@ class Program
 //    ENDIF;
 // ENDSUB;
  
-// thisFishState  "Fish";
-// thisFishSize  1;
-// OUTPUT thisFishState, " is of size ", thisFishSize;
-// WHILE thisFishState <> "FISH";
-//    feed(thisFishState, thisFishSize);
-// ENDWHILE;
-// OUTPUT "It is now a big ", thisFishState;
